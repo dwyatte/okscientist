5cb9447026b45a12c499196f15b27653365fda21
Dean Wyatte <dwyatte@setzer.local>
2012-10-09 00:25:06 -0600

    don't install deps since they are broken on Mac OS 10.8. Scipy Superpack anyway

--- deps/install_3rdparty.sh
+++ deps/install_3rdparty.sh
@@ -1,2 +1,5 @@
-sudo easy_install gensim
-sudo eash_install networkx
+# we already have the best dependencies for these from scipy superpack
+# and some of them won't build on Mac OS 10.8 anyway
+
+sudo easy_install --no-deps gensim
+sudo easy_install --no-deps networkx
