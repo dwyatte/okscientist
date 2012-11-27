96233047ba4270b469089e1f0cdad2b5471fc40c
Dean Wyatte <dwyatte@rl1-2-196-75-dhcp.int.colorado.edu>
2012-11-27 11:18:30 -0700

    thresholding, error-checking, vocab load from disk

--- demo.py
+++ demo.py
@@ -2,49 +2,66 @@ import nlpfuns
 import sys, os, subprocess
 import numpy
 import scipy.spatial.distance as distance
-import numpy.linalg as linalg
 import matplotlib.pyplot as plot
 
-#########################################################################################
-# TODO:
-# * make a BuildVocab wrapper in nlpfuns that returns/writes to disk
-# * figure out how to deal with bad pdfs -- probably just monitor stderr
-#########################################################################################
+# various flags/parameters
+DOC_ROOT = '/Users/dwyatte/Documents/Papers/'   # where to search for PDFs
+BUILD_VOCAB = 0                                 # whether to build vocab (slow) or read from disk
+VOCAB_FILE = 'vocab.dat'                        # vocab file to read from/write to disk
+WEIGHT_THRESH = 0.5                             # threshold for writing out an edge in output function
 
 if __name__ == '__main__':
-    vocab = []
-    stoplist = nlpfuns.ReadFlatText('stoplist.dat')
-    
-    # if we are on a mac, we can use the supplied pdftotext binary
+
+    # if we are on a Mac, we can use the supplied pdftotext binary
+    # TODO: make sure it is in our path, else error
     if sys.platform == 'darwin':
-        os.environ['PATH'] = 'bin:' + os.environ['PATH']
+        os.environ['PATH'] = 'bin:' + os.environ['PATH']         
         
-    # now make sure it is in our path -- TODO, else error
+    # gather up our doc paths 
+    docs = nlpfuns.FindPDFs(DOC_ROOT)
         
-    # gather up our docs from the test directory
-    docs = nlpfuns.FindPDFs('test')
-    #docs = nlpfuns.FindPDFs('/Users/dwyatte/Documents/Papers/')
         
-    # dump text and build vocab
-    print 'Building vocabulary (could take awhile)...'
-    for i,doc in enumerate(docs):
-        doctext = nlpfuns.DumpPDF(doc, filterjunk=True, stoplist=stoplist)
-        vocab = nlpfuns.UpdateVocab(doctext, vocab)
-        print '%d/%d' % (i, len(docs))
+    if BUILD_VOCAB:
+        vocab = []
+        stoplist = nlpfuns.ReadFlatText('stoplist.dat')    
+        print '\nBuilding vocabulary (could take awhile)...\n'
+        for i,doc in enumerate(docs, start=1):
+            print 'Adding text from %s (%d/%d)' % (doc, i, len(docs))
+            doctext = nlpfuns.DumpPDF(doc, filterjunk=True, stoplist=stoplist)
+            # if doc was not dumpable (i.e., image-based pdf, or copyrighted, etc.), rm from list
+            if len(doctext) > 0:
+                vocab = nlpfuns.UpdateVocab(doctext, vocab)
+            else:
+                print '|---> Could not dump text from this paper, skipping\n'
+                continue
+        nlpfuns.WriteFlatText(VOCAB_FILE, vocab)
+    else:
+        print '\nReading existing vocabulary from %s\n' % (VOCAB_FILE)
+        vocab = nlpfuns.ReadFlatText(VOCAB_FILE)
+        stoplist = nlpfuns.ReadFlatText('stoplist.dat')    
+ 
     
-    # compute features
+    # compute features, node labels
     features = [] 
-    for doc in docs:
-        print 'Computing features for %s' % doc
+    labels = []
+    print '\nComputing features (could take awhile)...'
+    for i,doc in enumerate(docs, start=1):
+        print 'Computing features from %s (%d/%d)' % (doc, i, len(docs))
         doctext = nlpfuns.DumpPDF(doc, filterjunk=True, stoplist=stoplist)
+        # if doc was not dumpable (i.e., image-based pdf, or copyrighted, etc.), rm from list
+        if len(doctext) > 0:
+            vocab = nlpfuns.UpdateVocab(doctext, vocab)
+        else:
+            print '|---> Could not dump text from this paper, skipping\n'
+            continue
         f = nlpfuns.BuildFeatures(doctext, vocab)
         f = numpy.array(f)
         # normalize
         f = (f-numpy.min(f)/(numpy.max(f)-numpy.min(f)))
-        features.append(f)        
-	    
+        features.append(f)
+        labels.append(doc.split(os.sep)[-1])      
+        	    
     distances = distance.pdist(features, 'cosine')
     distances = distance.squareform(distances)
-    
-    nlpfuns.WriteFlatText('vocab.dat', vocab)
-    nlpfuns.WriteGraphPajek('distances.net', docs, 1-distances)
+
+    nlpfuns.WriteGraphPajek('graph.net', labels, 1-distances, WEIGHT_THRESH)
--- nlpfuns.py
+++ nlpfuns.py
@@ -2,43 +2,54 @@
     parsing pdfs, building the vocabulary, features, etc. '''
 
 import subprocess, os, fnmatch
-import numpy
+
 
 ''' Loads and returns flat text file from disk, split by newlines '''
 def ReadFlatText(file):
     with open(file, 'r') as f:
          return f.read().split()        
+       
          
 ''' Writes list to disk (flat text, one word per line) '''
 def WriteFlatText(file, list):
     with open(file, 'w') as f:
         [f.write(x + '\n') for x in list]
+    return True
+       
         
-''' Writes out graph in pajek format. Uses distances as edge weights, which should
-    be symmetrical, so just write upper write triangle of matrix. Requires nupmy '''
-def WriteGraphPajek(netfile, labels, weights):
+''' Writes out graph in pajek format. Uses similarities as edge weights, which should
+    be symmetrical, so just write upper right triangle of matrix. Note that weights is a 
+    numpy array. thresh is a threshold that weight must surpass to get written as an edge '''
+def WriteGraphPajek(netfile, labels, weights, thresh=0.0):
     with open(netfile, 'w') as f:
+        # write node ids
         f.write('*vertices ' + str(len(labels)) + '\n')
-        for nodeid,label in enumerate(labels):
-            f.write(str(nodeid) + '\t' + '\"' + label + '\"\n')
-        
-        f.write('*arcs\n')    
+        for nodeid,label in enumerate(labels, start=1):
+            f.write(str(nodeid) + ' ' + '\"' + label + '\"\n')
+
+        # write the weights
+        f.write('*Edges\n')    
         for l1idx in range(0, len(labels)):
             for l2idx in range(l1idx+1, len(labels)):
-                f.write(str(l1idx) +  '\t' + str(l2idx) + '\t' + str(weights[l1idx, l2idx]) + '\n')
+                if weights[l1idx, l2idx] > thresh:
+                    f.write(str(l1idx+1) +  ' ' + str(l2idx+1) + ' ' + str(weights[l1idx, l2idx]) + '\n')
+  
+    return True
+            
             
 ''' Update the vocab with words (usually extracted from a pdf), and return new vocab '''
 def UpdateVocab(words, vocab):
 	for word in words:
 		if word not in vocab:
 			vocab.append(word)
+	
 	return vocab
 
 
 ''' dump contents of pdf at path using the binary pdftotext (assume binary is in path). '''
 def DumpPDF(path, filterjunk, stoplist):
     # third arg ('-') denotes that we are dumping the text to stdout, so we need to redirect
-    pdftotextProc = subprocess.Popen(['pdftotext', path, '-'], stdout=subprocess.PIPE)        
+    pdftotextProc = subprocess.Popen(['pdftotext', path, '-'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)        
     (stdoutdata, sterrdata) = pdftotextProc.communicate()
     stdoutdata = stdoutdata.lower().split()
     
@@ -56,10 +67,12 @@ def DumpPDF(path, filterjunk, stoplist):
 ''' Find all PDFs from root dir, return list of paths '''
 def FindPDFs(root):
     paths = []
+    
     for dirpath, dirnames, filenames in os.walk(root):
         for filename in fnmatch.filter(filenames, '*.pdf'):
             paths.append(os.path.join(dirpath, filename))  
-    return paths  
+    
+    return paths 
 
 
 ''' Build a feature vector from a list of words (usually extracted from a pdf), according
@@ -69,4 +82,5 @@ def BuildFeatures(words, vocab):
 	for word in words:
 	    if word in vocab:
     		features[word] +=1
+	
 	return features.values()
